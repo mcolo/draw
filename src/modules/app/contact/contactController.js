af6f018a77755ce62f6a4a623d11075c2857de13
Max Miracolo <max@assuredlabor.com>
2015-06-26 17:09:26 -0400

    set contact fields

--- src/modules/app/contact/contactController.js
+++ src/modules/app/contact/contactController.js
@@ -10,27 +10,22 @@ module.exports = /*@ngInject*/
         message: ''
     };
 
-    $scope.transform = function(obj) {
-        var str = [];
-        for(var p in obj);
-        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-        return str.join("&");
-    }
 
     $scope.submit = function(contactform) {
-        $scope.submitted = true;
-
-        var paramData =  Object.keys($scope.contact).map(function(k) {
-            return encodeURIComponent(k) + '=' + encodeURIComponent($scope.contact[k]);
-        }).join('&');
+        var url = '/assets/mail/contact-form.php';
+        
+        var str = [];
+        for(var p in contactform) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(contactform[p]));
+        }
+        var paramData = str.join("&");
 
         if (contactform.$valid) {
             $http({
                 method           : 'POST',
-                url              : '/contact-form.php',
-                headers          : { 'Content-Type': 'application/x-www-form-urlencoded' },  //set the headers so angular passing info as form data (not request payload)
-                transformRequest : $scope.transform($scope.contact),
-                data             : $scope.contact
+                url              : url,
+                data             : paramData,
+                headers          : { 'Content-Type': 'application/x-www-form-urlencoded' }
             }).success(function(data){
                 console.log(data);
                 if (data.success) { //success comes from the return json object
@@ -40,6 +35,10 @@ module.exports = /*@ngInject*/
                     $scope.result.message = data.message;
                     $scope.result.success=false;
                 }
+            }).error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $scope.result.message = data.message;
             });
         } else {
             $scope.result.message = 'Failed';
